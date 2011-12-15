0b6bbbd6ca481505ba2ca37fae6280fa80bdabb9
Mike Larsson <mikelarssonftw@gmail.com>
2011-12-15 12:57:47 -0500

    Changed fabfile.py so that the remote directory for deploy can be overridden from the commandline

--- fabfile.py
+++ fabfile.py
@@ -6,8 +6,8 @@ from fabric.contrib.project import rsync_project
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
 @task
-def deploy():
+def deploy(remote='/var/hackasaurus.org'):
     with lcd(ROOT):
         local('python manage.py build')
-        rsync_project(remote_dir='/var/hackasaurus.org',
+        rsync_project(remote_dir=remote,
                       local_dir='dist/')
